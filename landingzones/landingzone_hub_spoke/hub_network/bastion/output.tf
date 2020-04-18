d0ddc668e7f5d06ec9ad555836a066e24a471552
yingeli <yingeli@hotmail.com>
2020-04-18 11:05:13 +0800

    workaround enable_bastion not working issue

--- landingzones/landingzone_hub_spoke/hub_network/bastion/output.tf
+++ landingzones/landingzone_hub_spoke/hub_network/bastion/output.tf
@@ -1,4 +1,5 @@
 output "bastion" {
-  depends_on = [azurerm_bastion_host.azurebastion]
-  value = var.enable_bastion ? azurerm_bastion_host.azurebastion : null
+  //depends_on = [azurerm_bastion_host.azurebastion]
+  //value = var.enable_bastion ? azurerm_bastion_host.azurebastion : null
+  value = null
 }
